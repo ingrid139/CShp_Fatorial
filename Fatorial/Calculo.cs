b47bf4eb249736f4bb67f379d3d2dee5b02194a4
Ingrid Costa <ingrid139@hotmail.com>
2020-05-16 14:10:46 -0300

    add proj console

--- Fatorial/Calculo.cs
+++ Fatorial/Calculo.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 
 namespace Fatorial
 {
+    // classe de teste proposta pelo desafio
     public class Calculo
     {
+        //lógica a ser desenvolvida
         public long CalcularFatorial(int numero)
         {
+            // comparação numero menor que zero
             if (numero < 0)
             {
                 throw new ArgumentException("número inválido", nameof(numero));
             }
+            // comparação numero igual a zero
             else if (numero == 0)
             {
                 return 1;
@@ -18,12 +22,19 @@ namespace Fatorial
 
             long fatorial = numero;
 
+            //nova lista do tipo primitivo long para utilizar no retorno
             var listaFatorial = new List<long>();
 
+            // laço de repetição while - enquanto não satisfazer a condição a logica continua execuando o laço
             while (numero > 1)
             {
+                // add a variavel fatorial na lista
                 listaFatorial.Add(fatorial);
+
+                // multiplicar o valor da variavel fatorial pelo valor da variavel número
                 fatorial *= (numero - 1);
+
+                // decrementar o valor da variavel numero
                 numero--;
             }
 
---
+++ FatorialConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FatorialConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+            Console.WriteLine($"Cálculo fatorial do número: {numero}");
+
+            if (numero < 0)
+            {
+                Console.WriteLine($"número inválido: {numero}");
+            }
+            // comparação numero igual a zero
+            else if (numero == 0)
+            {
+                Console.WriteLine("O resultado é: 1");
+            }
+            else
+            {
+                long fatorial = numero;
+
+                //nova lista do tipo primitivo long para utilizar no retorno
+                var listaFatorial = new List<long>();
+
+                Console.WriteLine("***********************");
+                // laço de repetição while - enquanto não satisfazer a condição a logica continua execuando o laço
+                while (numero > 1)
+                {
+                    Console.WriteLine($"While --- antes do calculo : {fatorial}");
+                    // add a variavel fatorial na lista
+                    listaFatorial.Add(fatorial);
+
+                    // multiplicar o valor da variavel fatorial pelo valor da variavel número
+                    fatorial *= (numero - 1);
+
+                    Console.WriteLine($"While --- depois do calculo: {fatorial}");
+
+                    // decrementar o valor da variavel numero
+                    numero--;
+                    Console.WriteLine($"While --- valor numero: {numero}");
+                }
+
+                Console.WriteLine("***********************");
+
+                Console.WriteLine($"Resultado fatorial : {fatorial}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
